ee47768dc6fb1ae54f30d912d73f7bbd5c06e776
Juan Asas <juansimonasas@gmail.com>
2014-10-22 11:32:16 -0300

    #9 - Main layout :: The option by default must be "Tournaments" instead of "Users"

--- app/js/controllers/navCtrl.js
+++ app/js/controllers/navCtrl.js
@@ -1,21 +1,22 @@
 'use strict';
 
-app.controller ('navCtrl', function ($scope, $location){        
+app.controller ('navCtrl', function ($scope, $location){ 
+    
     $scope.changeState = function (optionNumber){
         if(optionNumber == 1){
-            $scope.isUserSelected = true;                
-            $scope.isTournamentSelected = false;
+            $scope.isTournamentSelected = true;
+            $scope.isUserSelected = false;                            
         }
         else{
-            $scope.isUserSelected = false;                
-            $scope.isTournamentSelected = true;
+            $scope.isTournamentSelected = false;
+            $scope.isUserSelected = true;                            
         }                
     }
     
     function init (){
         $location.path("/");
-        $scope.isUserSelected = true;                
-        $scope.isTournamentSelected = false;
+        $scope.isTournamentSelected = true;
+        $scope.isUserSelected = false;                        
     }
     
     init();
