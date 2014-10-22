20f0e31f7dc963baab113628b8264c10c197e77b
juanasas <juansimonasas@gmail.com>
2014-10-22 00:49:36 -0300

    003 - Users section :: Manage users - Add new user

--- web-server.js
+++ web-server.js
@@ -1,8 +1,13 @@
-var express = require("express"),
-    app     = express()
+var express = require("express");
+var http = require('http');
+var mongoose = require('mongoose');    
 var _ = require("underscore");
 
 //===========================CONFIGURATION==========================
+// Start express application
+var app = express();
+
+// all environments - configuration
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
     app.use(express.errorHandler());
@@ -22,4 +27,60 @@ app.get("/", function(req, res) {
 });
 
 //==================================================================
+//===========================MONGO DB===============================
+var db = mongoose.connection;
 
+db.on('error', console.error);
+
+db.once('open', function() {            
+    // Schema
+    var userSchema = new mongoose.Schema({        
+        name: { type: String },
+        lastname:  { type: String }
+    });
+    // Mongoose also creates a MongoDB collection called 'users' for these documents.
+    var users = mongoose.model('singleUser', userSchema);
+    
+    //===========================EXAMPLES===============================
+    
+    var user_example1 = new users({        
+        name: 'Ezequiel',
+        lastname:  'Peralta'
+    });
+    var user_example2 = new users({        
+        name: 'Negro',
+        lastname:  'Cardoso'
+    });
+    var user_example3 = new users({        
+        name: 'Juan',
+        lastname:  'Asas'
+    });
+    
+    user_example1.save();
+    user_example2.save();
+    user_example3.save();    
+    //==================================================================
+    
+    //===========================METHODS================================    
+    // get all users
+    app.get('/api/allUsers', function(req, res){
+        users.find(function(err, allUsers) {
+            if (err) return console.error(err);
+            res.send (allUsers);
+        });
+    });
+    
+    // create a new user
+    app.put('/newUser', function(req, res) {        
+        var newUser = new users({            
+            name : req.body.name,
+            lastname : req.body.lastname
+        });
+        newUser.save();
+        res.json(true);
+    });
+    //==================================================================        
+});
+//==================================================================
+
+mongoose.connect('mongodb://localhost/test');
